60d27b3cf51f9708acbce293953f3a8eb35c2166
CSE-ploss <emily@cse.camp>
2022-02-22 19:00:06 -0500

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -110,8 +110,12 @@ public class RobotContainer {
           // Start at the origin facing the +X direction
           new Pose2d(0, 0, new Rotation2d(0)),
           // List.of(new Translation2d(1, 1), new Translation2d(-2,1)),
-          List.of(),
-          new Pose2d(0, 3, new Rotation2d(0)),
+          List.of(
+            new Translation2d(0, 1),
+            new Translation2d(0, 0),
+            new Translation2d(-1, 0)
+          ),
+          new Pose2d(0, 0, new Rotation2d(0)),
           config);
     // }
 
